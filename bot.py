d204d4906bebd2c23ccfd00549251db2a669049f
Ass2013 <ant.ser.sol@gmail.com>
2025-12-06 11:51:01 +0100

    pls work

--- bot.py
+++ bot.py
@@ -28,6 +28,10 @@ def make_some(message):
     bot.send_message(message.chat.id, 'I accepted a new user!')
     bot.approve_chat_join_request(message.chat.id, message.from_user.id)
 
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
 
 
 bot.infinity_polling(none_stop=True)
